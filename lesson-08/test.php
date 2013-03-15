d1f6fc6b9a891d7c0fdc40fbdc402f51b56b4c3e
ah <webdstudent@gmail.com>
2013-03-14 20:27:21 -0700

    lesson 8 php upload attempt #2

--- lesson-08/test.php
+++ lesson-08/test.php
@@ -2,46 +2,50 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Music DB</title>
+<title>Lesson 8: CRUD App I</title>
 <link href="../../../bootstrap/css/bootstrap.css" rel="stylesheet">
 <link href="test.css" rel="stylesheet">
 </head>
 
 <body>
-   <div class="container">
-<h2>Lesson 7 Homework: Master Detail Database Structure</h2>
+<div class="container">
+<h3>Lesson 8 Homework: Master Detail Database Structure</h3>
 
 <table>
   <tr>
+    <th scope="col"><h4>Genre</h4></th>
     <th scope="col"><h4>Artist</h4></th>
     <th scope="col"><h4>Track</h4></th>
     </tr>
 
+<?php 
+require('connection.php');
+
+$sql = <<<SQL
+SELECT artist, track, genre
+FROM  genres, artists, albums, tracks
+WHERE ((genres.idgenres = artists.genres_idgenres) and 
+(artists.idartists = albums.artists_idartists) and 
+(albums.idalbums = tracks.albums_idalbums)) and 
+genres.idgenres in (1,2,3,4)  
+SQL;
+
+if(!$result = $db->query($sql)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+while($row = $result->fetch_assoc()){?>
+
+<tr><td class="span2"><?php echo $row['genre']; ?></td>
+<td><?php echo $row['artist']; ?></td>    
+<td><?php echo $row['track']; ?></td>
+
 <?php
-require('../lesson-07/connection.php');
-$music = $dataconnection->query("select artist from artists");
- while ($row = $music->fetch_object()) {
-	 ?>
-
-<tr><td>
-<?php echo $row->artist
-?>
-</td>
-
-<?php ;
 }
 
-$music = $dataconnection->query("select track from tracks");
- while ($row = $music->fetch_object()) {
-	 ?>
- 
-<td><?php echo $row->track ?></td>
-</tr>
 
-<?php ;
-}
-
-$dataconnection->close();
+$result->free();
+$db->close();
 ?>
 </table>
 
